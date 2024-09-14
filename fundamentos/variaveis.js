37e945a04f46d9f811a11bd5dd8479ace855bc7b
guisnt7 <gegeribeiropessoa@gmail.com>
2024-09-13 21:33:57 -0300

    estudo das variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,24 @@
+/**
+ * Estudo de constante e variáveis 
+ * @author guisnt7
+ */
+
+console.clear()
+
+// variáveis
+let nome 
+let idade
+let vip 
+
+
+nome = "guisnt7"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 19
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
